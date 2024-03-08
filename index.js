60ee0374cfe8c3f5976fe4ba64024d1d43909a48
agecode-debug <agecode95@gmail.com>
2024-03-08 13:54:40 +0100

    Formatage

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ const program = new Command();
 program.name("passwords-analyser");
 program.parse();
 
+// Create a loading spinner
 const loading = ora("Searching for passwords files").start();
 
+// Function to get password file from list
 async function getPasswordFileFromList(passwordFiles) {
   const passwordFileName = await inquirer.prompt([
     {
@@ -29,6 +31,7 @@ async function getPasswordFileFromList(passwordFiles) {
   return passwordFiles.find((file) => file.name === passwordFileName.name);
 }
 
+// Function to get password file from path
 async function getPasswordFileFromPath() {
   const passwordFilePath = await inquirer.prompt([
     {
@@ -51,23 +54,8 @@ async function getPasswordFileFromPath() {
   };
 }
 
-async function main() {
-  const { passwordFiles } = await searchPasswordFiles();
-
-  let passwordFile;
-
-  if (passwordFiles.length === 1) {
-    loading.succeed("Passwords file found !");
-    passwordFile = passwordFiles[0].path;
-  } else if (passwordFiles.length > 1) {
-    loading.succeed("Multiples passwords files found ! Please choose one");
-    passwordFile = await getPasswordFileFromList(passwordFiles);
-  } else {
-    loading.warn(
-      "No passwords files found ! Please manually specify the path to your passwords files.",
-    );
-    passwordFile = await getPasswordFileFromPath();
-  }
+// Function to start analysis
+async function startAnalysis(passwordFile) {
   const { format } = await import(
     `./passmanagers/${passwordFile.passManager}.js`
   );
@@ -76,10 +64,39 @@ async function main() {
   const loadingAnalysis = ora("Analyzing passwords").start();
   const results = analyze(format(jsonData));
   loadingAnalysis.succeed("Analysis done !");
-  const loadingServer = ora("Starting server").start();
-  startServer(results);
-  loadingServer.succeed("Server started !");
-  console.log("You can now see the results on http://localhost:3000");
+  return results;
+}
+
+// Main function
+async function main() {
+  try {
+    const { passwordFiles } = await searchPasswordFiles();
+
+    let passwordFile;
+
+    if (passwordFiles.length === 1) {
+      loading.succeed("Passwords file found !");
+      passwordFile = passwordFiles[0].path;
+    } else if (passwordFiles.length > 1) {
+      loading.succeed("Multiples passwords files found ! Please choose one");
+      passwordFile = await getPasswordFileFromList(passwordFiles);
+    } else {
+      loading.warn(
+        "No passwords files found ! Please manually specify the path to your passwords files.",
+      );
+      passwordFile = await getPasswordFileFromPath();
+    }
+
+    const results = await startAnalysis(passwordFile);
+
+    const loadingServer = ora("Starting server").start();
+    startServer(results);
+    loadingServer.succeed("Server started !");
+    console.log("You can now see the results on http://localhost:3000");
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
 }
 
 main();
