fde9c0c5d8e7028aec4b564c5a71aa2051f0329d
agecode-debug <agecode95@gmail.com>
2024-06-04 16:02:21 +0200

    Add static file serving for public directory

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
 
+const filename = fileURLToPath(import.meta.url);
+const dirnamePath = dirname(filename);
+
 function startServer(results) {
+  app.use(express.static(path.join(dirnamePath, "public")));
+
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 
-  app.get("/", (req, res) => {
+  app.get("/passwords", (req, res) => {
     res.send(results);
   });
 }
